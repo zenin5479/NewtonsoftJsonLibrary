e52beae94a4d03e39382f64e46793faebc513bb1
zenin5479 <zenin5479@gmail.com>
2026-01-18 22:45:40 +0300

    Update 0.8 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -8,47 +8,54 @@ namespace NewtonsoftJsonLibrary
    {
       static void Main()
       {
-         Console.WriteLine("Сериализация объекта");
-         // 1. Сериализация
-         Movie movie = new Movie { Id = 1, Title = "Миссия невыполнима" };
-         // movie - объект, преобразуем его в строку с помощью JsonConvert.SerializeObject           
-         string result = JsonConvert.SerializeObject(movie);
-         // Преобразуется в строку в виде {"Id": 1,"Title": "Миссия невыполнима"}
-         Console.WriteLine(result);
-
-         Console.WriteLine("\nДесериализация объекта");
-         // 2. Десериализация
-         Movie newMovie = JsonConvert.DeserializeObject<Movie>(result);
-         // Теперь строка преобразуется в объект           
-         Console.WriteLine("Id: " + newMovie.Id);
-         Console.WriteLine("Title: " + newMovie.Title);
-
-         Console.WriteLine("\nСериализация коллекции");
-         // 3. Сериализация коллекции
-         List<Movie> movies = new List<Movie>
-         {
-            new Movie{ Id=1, Title="Титаник" },
-            new Movie{ Id=2, Title="Марсианин"},
-            new Movie{ Id=3, Title="Черная пантера"},
-            new Movie{ Id=4, Title="Дэдпул 2"}
-         };
-
-         string collectionResult = JsonConvert.SerializeObject(movies);
-         Console.WriteLine(collectionResult);
-
-         Console.WriteLine("\nДесериализация коллекции");
-         // 4. Десериализация коллекции
-         List<Movie> newMovies = JsonConvert.DeserializeObject<List<Movie>>(collectionResult);
-         int i = 0;
-         while (i < newMovies.Count)
-         {
-            Movie item = newMovies[i];
-            Console.WriteLine("Id: " + item.Id + "; " + "Title: " + item.Title);
-            i++;
-         }
+         
 
          Console.ReadKey();
       }
+
+     void Case()
+     {
+        Console.WriteLine("Сериализация объекта");
+        // 1. Сериализация
+        Movie movie = new Movie { Id = 1, Title = "Миссия невыполнима" };
+        // movie - объект, преобразуем его в строку с помощью JsonConvert.SerializeObject           
+        string result = JsonConvert.SerializeObject(movie);
+        // Преобразуется в строку в виде {"Id": 1,"Title": "Миссия невыполнима"}
+        Console.WriteLine(result);
+
+        Console.WriteLine("\nДесериализация объекта");
+        // 2. Десериализация
+        Movie newMovie = JsonConvert.DeserializeObject<Movie>(result);
+        // Теперь строка преобразуется в объект           
+        Console.WriteLine("Id: " + newMovie.Id);
+        Console.WriteLine("Title: " + newMovie.Title);
+
+        Console.WriteLine("\nСериализация коллекции");
+        // 3. Сериализация коллекции
+        List<Movie> movies = new List<Movie>
+        {
+           new Movie{ Id=1, Title="Титаник" },
+           new Movie{ Id=2, Title="Марсианин"},
+           new Movie{ Id=3, Title="Черная пантера"},
+           new Movie{ Id=4, Title="Дэдпул 2"}
+        };
+
+        string collectionResult = JsonConvert.SerializeObject(movies);
+        Console.WriteLine(collectionResult);
+
+        Console.WriteLine("\nДесериализация коллекции");
+        // 4. Десериализация коллекции
+        List<Movie> newMovies = JsonConvert.DeserializeObject<List<Movie>>(collectionResult);
+        int i = 0;
+        while (i < newMovies.Count)
+        {
+           Movie item = newMovies[i];
+           Console.WriteLine("Id: " + item.Id + "; " + "Title: " + item.Title);
+           i++;
+        }
+      }
+
+
    }
 
    class Movie
