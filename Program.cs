9c1a4a6b8c21c93b1c15bb0a72f99e0c661c9d0b
zenin5479 <zenin5479@gmail.com>
2026-01-18 10:31:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewtonsoftJsonLibrary
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
