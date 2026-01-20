ea9b8b37f568821bdacfc0eedfeb5402e86ed9b7
zenin5479 <zenin5479@gmail.com>
2026-01-20 08:28:20 +0300

    Update 4.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ namespace NewtonsoftJsonLibrary
          // Десериализация полного объекта
          User jsondeserialized = JsonConvert.DeserializeObject<User>(jsonserializeformatting);
          Console.WriteLine(string.Format("Имя пользователя: {0}", jsondeserialized.UserName));
-         Console.WriteLine($"Email: {jsondeserialized.Email}");
+         Console.WriteLine(string.Format("Email: {0}", jsondeserialized.Email));
          Console.WriteLine($"Активен: {jsondeserialized.IsActive}");
          Console.WriteLine($"Роли: {string.Join(", ", jsondeserialized.Roles)}");
       }
