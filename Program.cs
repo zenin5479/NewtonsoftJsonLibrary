487b3c36f689a811ce13aaa473f6e83857eb8a6b
zenin5479 <zenin5479@gmail.com>
2026-01-23 09:29:49 +0300

    Update 12.9 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace NewtonsoftJsonLibrary
 
       static void CaseFour()
       {
+         Console.WriteLine("\nТочное время в Unix‑timestamp в миллисекундах (13‑значное число)");
          // 1. Через DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
          Console.WriteLine("1. Через DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()");
          long timestampOffset = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
