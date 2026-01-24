5f90192130c7efd6098389dd8c5682e32fb1d383
zenin5479 <zenin5479@gmail.com>
2026-01-24 11:30:05 +0300

    Update 18.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ namespace NewtonsoftJsonLibrary
          DateTimeOffset specificDate = DateTimeOffset.UtcNow;
          long timestamp3 = new DateTimeOffset(specificDate.UtcDateTime).ToUnixTimeMilliseconds();
          Console.WriteLine("Способ 3 (new DateTimeOffset().ToUnixTimeMilliseconds()):");
-         Console.WriteLine($"Результат: {timestamp3}");
+         Console.WriteLine("Результат: {0}", timestamp3);
          Console.WriteLine($"Длина: {timestamp3.ToString().Length} цифр");
          Console.WriteLine($"Формат: {timestamp3:#,##0}\n");
 
