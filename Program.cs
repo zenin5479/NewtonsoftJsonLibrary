7108d4ffb62ebc4186428c765717e97bbc0b0b7f
zenin5479 <zenin5479@gmail.com>
2026-01-27 09:10:06 +0300

    Update 18.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -126,7 +126,7 @@ namespace NewtonsoftJsonLibrary
          // Конвертация обратно для проверки
          Console.WriteLine("========================================================");
          Console.WriteLine("Конвертация обратно в DateTime:");
-         DateTimeOffset datefromtimestamp = DateTimeOffset.FromUnixTimeMilliseconds(timestampfive);
+         DateTimeOffset datefromtimestamp = DateTimeOffset.FromUnixTimeMilliseconds(timestamptwo);
          Console.WriteLine("Из timestamp: {0:yyyy-MM-dd HH:mm:ss.fff}", datefromtimestamp);
       }
 
